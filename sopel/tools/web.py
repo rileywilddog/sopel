020c954786cdd312e132a8bc822eb13056201d20
dgw <dgw@technobabbl.es>
2019-03-23 19:13:07 -0500

    web: Continued WIP move sopel.web -> sopel.tools.web
    Puts the deprecated functions back in sopel.web, and doesn't pull them
    back to the new location, so deprecated stuff will be left behind when
    module authors transition to the new import name
    
    None of this is tested at all, except for running checkstyle.sh. #noqa

--- sopel/tools/web.py
+++ sopel/tools/web.py
@@ -17,17 +17,13 @@ from __future__ import unicode_literals, absolute_import, print_function, divisi
 import re
 import sys
 import urllib
-import requests
 
 from sopel import __version__
-from sopel.tools import deprecated
 
 if sys.version_info.major < 3:
-    import httplib
     from htmlentitydefs import name2codepoint
     from urlparse import urlparse, urlunparse
 else:
-    import http.client as httplib
     from html.entities import name2codepoint
     from urllib.parse import urlparse, urlunparse
     unichr = chr
@@ -49,105 +45,6 @@ default_headers = {'User-Agent': USER_AGENT}
 ca_certs = None  # Will be overriden when config loads. This is for an edge case.
 
 
-class MockHttpResponse(httplib.HTTPResponse):
-    "Mock HTTPResponse with data that comes from requests."
-    def __init__(self, response):
-        self.headers = response.headers
-        self.status = response.status_code
-        self.reason = response.reason
-        self.close = response.close
-        self.read = response.raw.read
-        self.url = response.url
-
-    def geturl(self):
-        return self.url
-
-
-# HTTP GET
-@deprecated
-def get(uri, timeout=20, headers=None, return_headers=False,
-        limit_bytes=None, verify_ssl=True, dont_decode=False):  # pragma: no cover
-    """Execute an HTTP GET query on `uri`, and return the result. Deprecated.
-
-    `timeout` is an optional argument, which represents how much time we should
-    wait before throwing a timeout exception. It defaults to 20, but can be set
-    to higher values if you are communicating with a slow web application.
-    `headers` is a dict of HTTP headers to send with the request.  If
-    `return_headers` is True, return a tuple of (bytes, headers)
-
-    `limit_bytes` is ignored.
-
-    """
-    if not uri.startswith('http'):
-        uri = "http://" + uri
-    if headers is None:
-        headers = default_headers
-    else:
-        tmp = default_headers.copy()
-        tmp.update(headers)
-        headers = tmp
-    u = requests.get(uri, timeout=timeout, headers=headers, verify=verify_ssl)
-    bytes = u.content
-    u.close()
-    headers = u.headers
-    if not dont_decode:
-        bytes = u.text
-    if not return_headers:
-        return bytes
-    else:
-        headers['_http_status'] = u.status_code
-        return (bytes, headers)
-
-
-# Get HTTP headers
-@deprecated
-def head(uri, timeout=20, headers=None, verify_ssl=True):  # pragma: no cover
-    """Execute an HTTP GET query on `uri`, and return the headers. Deprecated.
-
-    `timeout` is an optional argument, which represents how much time we should
-    wait before throwing a timeout exception. It defaults to 20, but can be set
-    to higher values if you are communicating with a slow web application.
-
-    """
-    if not uri.startswith('http'):
-        uri = "http://" + uri
-    if headers is None:
-        headers = default_headers
-    else:
-        tmp = default_headers.copy()
-        tmp.update(headers)
-        headers = tmp
-    u = requests.get(uri, timeout=timeout, headers=headers, verify=verify_ssl)
-    info = u.headers
-    u.close()
-    return info
-
-
-# HTTP POST
-@deprecated
-def post(uri, query, limit_bytes=None, timeout=20, verify_ssl=True, return_headers=False):  # pragma: no cover
-    """Execute an HTTP POST query. Deprecated.
-
-    `uri` is the target URI, and `query` is the POST data.
-
-    If `return_headers` is true, returns a tuple of (bytes, headers).
-
-    `limit_bytes` is ignored.
-
-    """
-    if not uri.startswith('http'):
-        uri = "http://" + uri
-    u = requests.post(uri, timeout=timeout, verify=verify_ssl, data=query)
-    bytes = u.raw.read(limit_bytes)
-    headers = u.headers
-    u.close()
-    if not return_headers:
-        return bytes
-    else:
-        headers['_http_status'] = u.status_code
-        return (bytes, headers)
-
-
 r_entity = re.compile(r'&([^;\s]+);')
 
 
@@ -164,30 +61,6 @@ def entity(match):
 
 def decode(html):
     return r_entity.sub(entity, html)
-
-
-# For internal use in web.py, (modules can use this if they need a urllib
-# object they can execute read() on) Both handles redirects and makes sure
-# input URI is UTF-8
-@deprecated
-def get_urllib_object(uri, timeout, headers=None, verify_ssl=True, data=None):  # pragma: no cover
-    """Return an HTTPResponse object for `uri` and `timeout` and `headers`. Deprecated
-
-    """
-
-    if headers is None:
-        headers = default_headers
-    else:
-        tmp = default_headers.copy()
-        tmp.update(headers)
-        headers = tmp
-    if data is not None:
-        response = requests.post(uri, timeout=timeout, verify=verify_ssl,
-                                 data=data, headers=headers)
-    else:
-        response = requests.get(uri, timeout=timeout, verify=verify_ssl,
-                                headers=headers)
-    return MockHttpResponse(response)
 
 
 # Identical to urllib2.quote
